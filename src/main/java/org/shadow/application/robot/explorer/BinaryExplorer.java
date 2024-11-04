c66216194ff18c92775ab72ebd929daf8477b66b
Alexander <41678109+WhoIsDarth@users.noreply.github.com>
2024-11-04 22:33:10 +0100

    feat: add java docs

--- src/main/java/org/shadow/application/robot/explorer/BinaryExplorer.java
+++ src/main/java/org/shadow/application/robot/explorer/BinaryExplorer.java
@@ -5,13 +5,39 @@ import org.shadow.application.robot.common.model.Bar;
 import org.shadow.application.robot.explorer.model.BinaryIsMomentumExplorationState;
 import org.shadow.application.robot.indicator.Indicator;
 
+/**
+ * This interface represents a binary explorer that evaluates whether the market is in a momentum
+ * phase to either go long or short based on a specific indicator.
+ */
 public interface BinaryExplorer {
 
+  /**
+   * Evaluates whether the market is in a momentum phase to go long based on the indicator.
+   *
+   * @param bars the list of bars to evaluate
+   * @return the exploration state indicating whether the market is in a momentum phase to go long
+   */
   BinaryIsMomentumExplorationState isMomentumToLong(List<Bar> bars);
 
+  /**
+   * Evaluates whether the market is in a momentum phase to go short based on the indicator.
+   *
+   * @param bars the list of bars to evaluate
+   * @return the exploration state indicating whether the market is in a momentum phase to go short
+   */
   BinaryIsMomentumExplorationState isMomentumToShort(List<Bar> bars);
 
+  /**
+   * Gets the severity level of momentum exploration.
+   *
+   * @return the severity level of momentum exploration
+   */
   Integer getSeverity();
 
+  /**
+   * Gets the indicator used by the explorer.
+   *
+   * @return the indicator used by the explorer
+   */
   Indicator getIndicator();
 }
